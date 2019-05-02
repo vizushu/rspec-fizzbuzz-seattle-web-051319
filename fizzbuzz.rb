f24a97d1dd882b375d5dfeb242fcfe1d2edf5f25
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-02 02:01:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -14,12 +14,12 @@
 # end
 
 def fizzbuzz(num)
-  if num / 3 == 0  && num / 5 == 0
+  if num % 3 == 0  && num % 5 == 0
     return "FizzBuzz"
   elsif
-     num / 3 == 0
+     num % 3 == 0
     return "Fizz"
-  elsif num / 5 == 0
+  elsif num % 5 == 0
      return "Buzz"
   else nil
    end
