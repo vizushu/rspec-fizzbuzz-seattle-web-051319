9b9c46d0a08f1f3a8759960ce1c5baafa61049c3
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-02 01:42:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-    if int % 3 == 0
-        puts "Fizz"
-    end
-    if int % 5 == 0
-        puts "Buzz"
-    end
-    if (int % 3 == 0) && (int % 5 == 0)
-        puts "FizzBuzz"
-    end
+def fizzbuzz(fizz)
+  if fizz / 3
+    puts "Fizz"
+  end
+  if fizz / 5
+    puts "Buzz"
+  end
+  if fizz / 5 || 3
+    puts "FizzBuzz"
+  end
 end
