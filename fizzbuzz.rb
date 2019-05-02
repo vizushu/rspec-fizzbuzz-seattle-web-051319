a2585fe012048d21f313230d4f76995079faed4e
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-02 01:27:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(Z)
-  if Z / 3
+def fizzbuzz(fizz)
+  if fizz / 3
     return "Fizz"
+  elsif fizz / 5
+    return "Buzz"
+  else 
 end
